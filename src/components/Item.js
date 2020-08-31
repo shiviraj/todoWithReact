ea5e89ca5ca1bc54fd4a7381e978822897151ed5
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-08-31 12:59:06 +0530

    Added one more state in status of todo

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => (
   <div className="item">
-    <div className={props.value.complete ? 'complete' : 'incomplete'}></div>
+    <div className={props.value.status}></div>
     <div onClick={props.handleClick}>{props.value.name}</div>
   </div>
 );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,13 +6,21 @@ class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.state = { items: [] };
+    this.statuses = ['todo', 'doing', 'done'];
+    this.defaultStatus = 'todo';
     this.handleAddItem = this.handleAddItem.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
+  updateState(status) {
+    const index = this.statuses.findIndex((item) => item === status);
+    const nextIndex = (index + 1) % this.statuses.length;
+    return this.statuses[nextIndex];
+  }
+
   handleClick(id) {
     const newItems = this.state.items.map((item, index) => {
-      if (id === index) item.complete = !item.complete;
+      if (id === index) item.status = this.updateState(item.status);
       return item;
     });
     this.setState({ items: newItems });
@@ -20,7 +28,7 @@ class TodoList extends React.Component {
 
   handleAddItem(item) {
     this.setState(({ items }) => ({
-      items: items.concat({ name: item, complete: false }),
+      items: items.concat({ name: item, status: this.defaultStatus }),
     }));
   }
 
